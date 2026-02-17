3fbf0a2020bb44f59d4ef48bce79577d6a6003db
Pradum Kumar <pradumky803@gmail.com>
2026-02-18 00:21:22 +0530

    clearing

--- components/achievements/achievement.tsx
+++
@@ -1,7 +0,0 @@
-import { AchievementClient } from "@/components/achievements/achievement-client";
-import { getAchievementData } from "@/lib/achievements-data";
-
-export async function Achievement() {
-  const data = await getAchievementData();
-  return <AchievementClient data={data} />;
-}
